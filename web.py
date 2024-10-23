1ec40553e21b5971efdc96e44dc021e65aa56441
sharon120 <sharonharshini@gmail.com>
2024-10-23 12:54:46 +0530

    Success

---
+++ web.py
@@ -0,0 +1,110 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+content="""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laptop Configuration</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color:gainsboro;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        table {
+            width: 70%;
+            margin: 50px auto;
+            border-collapse: collapse;
+            background-color: #fff;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        th, td {
+            padding: 15px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color:gray;
+            color: white;
+        }
+
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+
+        td {
+            color: #333;
+        }
+
+        .highlight {
+            background-color: #e0f7fa;
+        }
+    </style>
+</head>
+<body>
+
+<h1>SHARON HARSHINI L M</h1>
+<h1>212223040193</h1>
+<h1>Laptop Configuration</h1>
+
+<table>
+    <tr>
+        <th>Component</th>
+        <th>Details</th>
+    </tr>
+    <tr>
+        <td>Model</td>
+        <td>Lenovo Thinkpad</td>
+    </tr>
+    <tr>
+        <td>Processor</td>
+        <td>12th Gen Intel(R) Core(TM) i5-1235U   1.30 GHz</td>
+    </tr>
+    <tr>
+        <td>RAM</td>
+        <td>16 GB DDR4</td>
+    </tr>
+    <tr>
+        <td>Storage</td>
+        <td>512 GB SSD</td>
+    </tr>
+    <tr>
+        <td>Graphics</td>
+        <td>Intel Iris XE</td>
+    </tr>
+    <tr>
+        <td>Display</td>
+        <td>15.6" 2K</td>
+    </tr>
+    <tr>
+        <td>Battery</td>
+        <td>86 Whr</td>
+    </tr>
+</table>
+
+</body>
+</html>
+"""
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("Get request received...")
+        self.send_response(200) 
+        self.send_header("content-type", "text/html")       
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+print("This is my webserver") 
+server_address =('',8000)
+httpd = HTTPServer(server_address,MyServer)
+httpd.serve_forever()
